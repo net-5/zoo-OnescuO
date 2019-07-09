53bf275f37f91951866d51c72e9d8b1a6d90a553
OnescuO <onescu.oana@gmail.com>
2019-07-09 16:08:49 +0300

    versiunea-1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TemaZoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zoo zoo = new Zoo("NewYork zoo");
+            zoo.AddAnimal("wolf", "sheep");
+            zoo.AddAnimal("fox", "chicken");
+            zoo.AddAnimal("lion", "meat");
+            zoo.AddAnimal("monkey", "bananas");
+            zoo.AddAnimal("horse1", "carrots");
+            zoo.AddAnimal("horse2", "apples");
+            zoo.PresentZoo();
+            zoo.Transfer("fox");
+            zoo.PresentZoo();
+        }
+    }
+}
